5d555cc19e635ad594d597b62a4bc37404c60c1c
Dhanush Kiran K <dhanuu08@gmail.com>
2019-10-13 10:39:10 +0530

    SASS

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,6 +1,6 @@
 module.exports.home = function(req,res){
     console.log(req.cookies)
-    res.cookie('user_id',01);
+    //res.cookie('user_id',01);
     res.render('home',{
         title : "home",
     })
--- routes/users.js
+++ routes/users.js
@@ -3,17 +3,18 @@ const router = express.Router();
 const userController = require('../controllers/users_controller.js');
 const passport = require('passport');
 
-router.use('/profile',passport.checkAuthentication,userController.profile);
-router.use('/posts',userController.posts);
-router.use('/signup',userController.signup);
-router.use('/signin',userController.signin);
+router.get('/profile',passport.checkAuthentication,userController.profile);
+router.get('/posts',userController.posts);
+router.get('/signup',userController.signup);
+router.get('/signin',userController.signin);
 
 router.post('/create',userController.create)
 
 //using passport as middlewzare to autenticate
 router.post('/create-session',passport.authenticate(
     'local',
-    {failureRedirect:'/users/signin'},
+    {failureRedirect:'/users/signin',
+    failureFlash:true},
 ),userController.createSession)
 
 router.get('/signout',userController.destroySession);
